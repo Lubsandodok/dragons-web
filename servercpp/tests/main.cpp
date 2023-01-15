dc2e28e906595ab4b8af88349a635d2ae15342a0
Daniel Brovtcev <lubsandodok@gmail.com>
2023-01-15 13:47:42 +0400

    change cpp integration test client to poco instead of easywsclient

--- servercpp/tests/main.cpp
+++ servercpp/tests/main.cpp
@@ -1,59 +1,85 @@
+#include <exception>
 #include <iostream>
-#include <string>
-#include <vector>
+#include <sstream>
 #include <thread>
 
-#include <ostream>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/Net/HTTPMessage.h>
+#include <Poco/Net/WebSocket.h>
+#include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Buffer.h>
+#include <Poco/StreamCopier.h>
+#include <string>
+
 #include "include/json.hpp"
-#include "include/easywsclient.hpp"
-#include "include/httplib.h"
 
+using namespace Poco::Net;
 using namespace nlohmann;
-using easywsclient::WebSocket;
 
-void handle_message(const std::string& message) {
-    std::cout << message << std::endl;
+std::string create_room() {
+    HTTPClientSession session("localhost", 9001);
+    HTTPRequest request(HTTPRequest::HTTP_POST, "/room/create", HTTPMessage::HTTP_1_1);
+
+    json body_to_create = {{"expected_player_count", 2}};
+
+    request.setContentType("application/json");
+    request.setContentLength(body_to_create.dump().length());
+
+    std::ostream& os = session.sendRequest(request);
+    os << body_to_create.dump();
+
+    HTTPResponse response;
+    std::cout << response.getStatus() << " " << response.getReason() << std::endl;
+
+    std::istream& is = session.receiveResponse(response);
+    std::stringstream ss;
+    Poco::StreamCopier::copyStream(is, ss);
+
+    std::cout << ss.str() << std::endl;
+
+    json responseObject = json::parse(ss.str());
+
+    return responseObject["room_id"];
 }
 
-WebSocket::pointer create_ws_connection(const std::string& room_id) {
-    WebSocket::pointer ws = WebSocket::from_url("ws://localhost:9001/room/join");
+void create_ws_connection(const std::string& room_id) {
+    HTTPClientSession session("localhost", 9001);
+    HTTPRequest request(HTTPRequest::HTTP_GET, "/room/join", HTTPMessage::HTTP_1_1);
+    HTTPResponse response;
 
     json body_to_join = {
         {"method", "JOIN_ROOM"},
         {"parameters", {{"room_id", room_id}}},
     };
+    std::string message = body_to_join.dump();
+    std::cout << message << std::endl;
 
-    ws->send(body_to_join.dump());
+    try {
+        WebSocket* ws = new WebSocket(session, request, response);
+        // dangerous place. Maybe length() is not possible to use here.
+        int length = ws->sendFrame(message.c_str(), message.length(), WebSocket::FRAME_TEXT);
+        int flags = 0;
 
-    while (true) {
-        ws->poll();
-        ws->dispatch(handle_message);
+        int responsePayloadSize = 2000;
+        Poco::Buffer<char> buffer(0);
+        while (true) {
+            buffer.clear();
+            length = ws->receiveFrame(buffer, flags);
+            if (length != 0) {
+                std::string responseString(buffer.begin(), buffer.end());
+                std::cout << "Length: " << length << "Got: " << responseString << std::endl;
+            }
+        }
+
+    } catch (std::exception& exc) {
+        std::cout << "Exception: " << exc.what() << std::endl;
     }
-    return ws;
 }
 
-int main() {
-    std::cout << "Start" << std::endl;
+int main(int argc, char** argv) {
 
-    httplib::Client cli("http://localhost:9001");
-    cli.set_logger([] (const httplib::Request& req, const httplib::Response& res) {
-        std::cout << "Log" << req.method << std::endl;
-    });
-
-    json body_to_create = {
-        {"expected_player_count", 2},
-    };
-    std::cout << "Before" << body_to_create.dump() << std::endl;
-    httplib::Result res = cli.Post("/room/create", body_to_create.dump(), "application/json");
-    if (!(res && res.value().status == 200)) {
-        std::cout << "Answer is not fine" << std::endl;
-        return 0;
-    }
-
-    json response_body = json::parse(res->body);
-    std::string room_id = response_body["room_id"];
-
-    std::cout << "Answer: " << res->body << "Room id" << room_id << std::endl;
+    std::string room_id = create_room();
 
     std::vector<std::thread> threads;
     threads.push_back(std::thread(create_ws_connection, room_id));
